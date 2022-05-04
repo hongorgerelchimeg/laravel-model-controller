114f0c2dc09d013643e579d171e52cd41dfc30d7
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-05-04 21:08:53 +0200

    index public methods created

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        $movies = Movie::all();
+        return view('home', [ 'movies' => $movies]);
+    }
 }
